3cb8f49e58ebc2ed06770f1a56f17ce8366d7fb4
victhorplus <victhor-hugo-11@hotmail.com>
2020-03-20 09:59:47 -0300

    mock-heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HERO: Hero[] = [
+    {id: 1, name: 'Magneto', img: 'assets/img/magneto.png'},
+    {id: 2, name: 'Tocha Humana', img: 'assets/img/tocha-humana.png'},
+    {id: 3, name: 'Capitão América', img: 'assets/img/capitao-america.png'},
+    {id: 4, name: 'Star Lord', img: 'assets/img/star-lord.png'},
+    {id: 5, name: 'Rocket Racoon', img: 'assets/img/rocket.png'},
+    {id: 6, name: 'Miranha', img: 'assets/img/miranha.png'},
+    {id: 7, name: 'Hulk', img: 'assets/img/hulk.png'},
+    {id: 8, name: 'Sandman', img: 'assets/img/sandman.png'},
+    {id: 9, name: 'Quicksilver', img: 'assets/img/quicksilver.png'},
+    {id: 10, name: 'Groot', img: 'assets/img/groot.png'},
+    {id: 11, name: 'Windstorm', img: ''},
+]
